0f2d748d1fa496fd7dd89a54422987de9113b88c
Mauricio Lopez <155264031+mauriloopezb17@users.noreply.github.com>
2025-12-14 23:26:24 -0400

    Se implementó el algoritmo de ordenación de los artículos para el momento de mandarlos al frontend estos estén ordenados según su id y sea más estructurada su respuesta.

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title=settings.APP_NAME)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], # Frontend URL
+    allow_origins=["*"],  # Frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
